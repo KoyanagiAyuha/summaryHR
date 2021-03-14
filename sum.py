60110f8f9190e66a8199fb68eb5c1f8012bbfa34
KoyanagiAyuha <ayuha.koyanagi@gmail.com>
2021-03-14 20:50:33 +0900

    firstcommit

---
+++ sum.py
@@ -0,0 +1,51 @@
+import boto3
+import tempfile
+import os
+import cv2
+import logging
+from botocore.exceptions import ClientError
+
+
+BUCKET = os.environ.get('BUCKET')
+S3PATH = os.environ.get('S3PATH')
+DB = os.environ.get('DB')
+
+
+def fin_check():
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(DB)
+    try:
+        response = table.get_item(Key={'S3PATH': S3PATH})
+    except ClientError as e:
+        logging.error(e.response['Error']['Message'])
+    else:
+        return 'Item' in response
+
+def time_put(sec):
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table(DB)
+
+    response = table.put_item(Item={'User':'SyanaiShikaku', 'S3PATH': S3PATH, 'time': sec})
+
+
+    return response
+
+if __name__ == "__main__":
+    if fin_check():
+        logging.critical('{} is already dynamoDB')
+    else:
+        tmpdir = tempfile.TemporaryDirectory()
+        tmp = tmpdir.name + '/'
+        tmp_file = tmp + 'tmp.mp4'
+        s3 = boto3.client('s3')
+        bucket = s3.Bucket(BUCKET)
+        try:
+            bucket.download_file(S3PATH, tmp_file)
+            cap = cv2.VideoCapture(tmp_file)                  # 動画を読み込む
+            video_frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT) # フレーム数を取得する
+            video_fps = cap.get(cv2.CAP_PROP_FPS)                 # フレームレートを取得する
+            video_len_sec = video_frame_count / video_fps
+            response = time_put(video_len_sec)
+        except:
+            logging.error('error_{}'.format(S3PATH))
